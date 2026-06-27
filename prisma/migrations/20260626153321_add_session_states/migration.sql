620b0f2ea3b52a0bd56b045a4c750868386f7473
Divine "KamCy" Ahaka <69519905+KamcyAhaka@users.noreply.github.com>
2026-06-27 14:45:14 +0100

    feat(schema): add new session states for KYC and onboarding

---
+++ prisma/migrations/20260626153321_add_session_states/migration.sql
@@ -0,0 +1,14 @@
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "SessionState" ADD VALUE 'KYC_NAME';
+ALTER TYPE "SessionState" ADD VALUE 'KYC_EMAIL';
+ALTER TYPE "SessionState" ADD VALUE 'INTENT_SELECTION';
+ALTER TYPE "SessionState" ADD VALUE 'ONBOARDING_BUSINESS_NAME';
+ALTER TYPE "SessionState" ADD VALUE 'ONBOARDING_COMPLETE';
+ALTER TYPE "SessionState" ADD VALUE 'CUSTOMER_BROWSING';
